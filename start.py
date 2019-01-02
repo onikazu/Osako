a6dbc2cfbf288b3d2c4bc7fd4ff33b26eaadd1f9
onikazu <programingmanagement@gmail.com>
2019-01-02 18:17:35 +0900

    revise start.py

--- start.py
+++ start.py
@@ -1,9 +1,7 @@
 import subprocess
 
-cmd1 = "rcsssserver"
-cmd2 = "rcssmonitor"
-cmd3 = "python base_client.py"
+cmd1 = "rcssoccersim"
+cmd2 = "python base_client.py"
 
 subprocess.Popen(cmd1.split())
 subprocess.Popen(cmd2.split())
-subprocess.Popen(cmd3.split())
